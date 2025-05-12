8a21b89434c128fa458f3abd0eaf49a8650b03c7
SJay20 <66739984+SJay20@users.noreply.github.com>
2025-05-12 01:01:08 -0400

    3 servos

--- pca9685_servo_test.py
+++ pca9685_servo_test.py
@@ -3,19 +3,33 @@ from time import sleep
 
 kit = ServoKit(channels=16)
 
-kit.servo[0].actuation_range = 270
-kit.servo[0].set_pulse_width_range(500, 2500)
+servo1 = kit.servo[0]
+servo2 = kit.servo[1]
+servo3 = kit.servo[2]
+
+servo1.actuation_range = 270
+servo1.set_pulse_width_range(500, 2500)
+
+servo2.actuation_range = 270
+servo2.set_pulse_width_range(500, 2500)
+
+servo3.actuation_range = 270
+servo3.set_pulse_width_range(500, 2500)
 
 while(True):
-    kit.servo[0].angle = 0
-    print("0")
+    servo1.angle = 0
+    servo2.angle = 90
+    servo2.angle = 180
     sleep(2)
-    kit.servo[0].angle = 90
-    print("90")
+    servo1.angle = 90
+    servo2.angle = 180
+    servo2.angle = 270
     sleep(2)
-    kit.servo[0].angle = 180
-    print("180")
+    servo1.angle = 180
+    servo2.angle = 270
+    servo2.angle = 0
     sleep(2)
-    kit.servo[0].angle = 270
-    print("270")
+    servo1.angle = 270
+    servo2.angle = 0
+    servo2.angle = 90
     sleep(2)

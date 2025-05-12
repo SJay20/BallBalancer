eacded1c15048606248d980e98d79fd41ecacf13
Sanjay Tamil <sanjaytamil19@gmail.com>
2025-05-11 23:56:14 -0400

    working pwm code

--- servotest.py
+++ servotest.py
@@ -1,12 +1,15 @@
+from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(18, min_angle=0, max_angle=270, min_pulse_width=0.0005, max_pulse_width=0.0025)
+servo = AngularServo(18, min_angle=0, max_angle=270, min_pulse_width=0.0004, max_pulse_width=0.0025, pin_factory=PiGPIOFactory())
 
-while (True):
+while(True):
     servo.angle = 0
     sleep(2)
-    servo.angle = 135
+    servo.angle = 90
     sleep(2)
-    servo.angle = 260
+    servo.angle = 180
+    sleep(2)
+    servo.angle = 270
     sleep(2)
